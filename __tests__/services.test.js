9d1fcaad13a950bb75003a26b7cdd08c92809b74
Sanderson Silva Corrêa <correasanderson@gmail.com>
2020-08-15 14:40:40 -0300

    implementação da função hasOwnProperty nos testes

--- __tests__/services.test.js
+++ __tests__/services.test.js
@@ -2,14 +2,29 @@ require('dotenv').config()
 import getReceitas from '../src/services/RecipePuppey'
 import getGif from '../src/services/Giphy'
 
+test('Deve ter a propriedade title', async () => {
+    const data = await getReceitas('bacon')
+    expect(data.hasOwnProperty('title')).toBe(true)
+})
+
 test('Deve retornar o title preenchido como Recipe Puppy', async () => {
     const data = await getReceitas('bacon')
     expect(data.title).toBe('Recipe Puppy')
 })
 
+test('Deve ter a propriedade Results', async () => {
+    const data = await getReceitas('bacon')
+    expect(data.hasOwnProperty('results')).toBe(true)
+})
+
 test('Deve ter o campo Results no objeto', async () => {
     const data = await getReceitas('bacon')
     expect(typeof data.results).toBe('object')
+})
+
+test('Deve ter a propriedade data', async () => {
+    const result = await getGif('bacon')
+    expect(result.hasOwnProperty('data')).toBe(true)
 })
 
 test('Deve ter o campo data no objeto', async () => {
